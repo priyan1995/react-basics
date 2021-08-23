e64a64a2bdf70595005d68cdb711220cf5b10ce3
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-23 23:49:30 +0530

    click event

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,18 @@ import React from 'react';
 class Home extends React.Component{
     
     render(){
-        const content = 'Home page content here';
+        // const content = 'Home page content here';
         // const link = "https://www.youtube.com";
+        const handleClick = () =>{
+            console.log('Hello There');
+        }
+
+
         return(
-            <h1>{ content }</h1>
-            
+            // <h1>{ content }</h1>            
             // <a href={ link }>Watch react tutorials here</a>
+            
+            <button onClick={handleClick}>Click Me</button>
         )
     }
 }
