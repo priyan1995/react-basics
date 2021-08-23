e49a99dcec0de93ef86e3995123fe900ef916e76
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-23 10:31:24 +0530

    pages added

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component{
+    
+    render(){
+        const content = 'Home page content here';
+        return(
+            <h1>{ content }</h1>
+        )
+    }
+}
+
+export default Home;
---
+++ src/pages/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <nav className="navbar">
+                <h1>Lorem Ipsum</h1>
+                <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/create">New Blog</a>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
