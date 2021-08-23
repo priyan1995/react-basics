99ffdcc3223dc4f9bc704416f735158cb82c5a6c
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-23 10:37:59 +0530

    adding urls

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,11 @@ class Home extends React.Component{
     
     render(){
         const content = 'Home page content here';
+        // const link = "https://www.youtube.com";
         return(
             <h1>{ content }</h1>
+            
+            // <a href={ link }>Watch react tutorials here</a>
         )
     }
 }
