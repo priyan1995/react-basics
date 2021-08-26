9d8829ef5922810a02d58d287bfcfea5296f2390
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-26 20:27:19 +0530

    get data from API

--- src/pages/userData.js
+++ src/pages/userData.js
@@ -2,13 +2,54 @@ import { useEffect, useState } from "react";
 
 const UserData = () => {
 
-    useEffect( () => {
-        fetch
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                // const blogid = (data.id);
+                // console.log(blogid);
+                // const title = data.title;
+                setUsers(data);
+
+
+            })
+        // console.log(users);
+
     })
 
-    return(
+
+    return (
         <div>
-        <h1>User Data</h1>
+            <h1>User Data</h1>
+            <hr />
+            
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <th scope="row">{ users.id }</th>
+                        <td></td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+
         </div>
     );
 
