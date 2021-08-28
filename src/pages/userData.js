5913b052ff73ad3bd4c1602972a151def5f6964a
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-28 18:39:22 +0530

    made custom hook

--- src/pages/userData.js
+++ src/pages/userData.js
@@ -1,36 +1,9 @@
 import { useEffect, useState } from "react";
+import useFetch from "../hooks/fetchData";
 
 const UserData = () => {
     
-    const [users, setUsers] = useState([]);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(true);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(res => {               
-                if(!res.ok){
-                    throw Error('Could not fetch the data for that resource..');
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                // const blogid = (data.id);
-                // console.log(blogid);
-                // const title = data.title;
-                setUsers(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                console.log(err.message);
-                setError(err.message);
-                setIsPending(false);
-            })
-        // console.log(users);
-
-    })
+   const  {data: users , isPending, error} = useFetch('https://jsonplaceholder.typicode.com/todos/1');
 
 
     return (
