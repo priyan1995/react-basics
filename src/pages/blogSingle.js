6af25ac5ac38f46e7c311b7656f0900ddc55b6e0
priyan <45042030+priyan1995@users.noreply.github.com>
2021-08-30 23:46:15 +0530

    blog sigle page url params added for fetch

--- src/pages/blogSingle.js
+++ src/pages/blogSingle.js
@@ -1,10 +1,28 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const BlogDetails = () =>{
-    const { id } = useParams();
+    
+    
+    const [blogs] = useState([
+        { title: "My new Website", body: "Lorem ipsum", author: "Mario", id: 1 },
+        { title: "Welcome to the Party", body: "Doler sit", author: "Doe", id: 2 },
+        { title: "Web dev tool tips", body: "Amet ipsum", author: "Mario", id: 3 },
+    ]);
+
+    const { id } = useParams(); // catch the ID from url
+    
+
     return (
         <div className="blog-details">
-            <h2>Blog View - { id }</h2>
+            {blogs.filter(blogs => blogs.id == id).map(filteredBlog =>(
+                <div>
+                    <h2>{ filteredBlog.title }</h2>
+                    <p>{ filteredBlog.body }</p>
+                    <h6>Author: { filteredBlog.author }</h6>
+                </div>
+            ))}
+            
         </div>
     );
 }
