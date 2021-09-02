f41f7d8b1e5f1c98371c642c6fd6636c3ab29473
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-02 20:01:29 +0530

    react forms

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
 
 
 const CreateBlog = () => {
-    return(
-        <div className="create-blog">
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+    return (
+        <div className="create">
             <h2>Add New Blog</h2>
+            <form>
+
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                    value = {title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog Body</label>
+                <textarea
+                 required
+                 value = {body}
+                 onChange = {(e)=> setBody(e.target.value) }
+                 ></textarea>
+
+                <label>Blog Author</label>
+                <select
+                value = {author}
+                onChange = {(e)=> setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Doe">Doe</option>
+                </select>
+
+                <button>Add Blog</button>
+
+            </form>
+
+            <h4>Title: {title}</h4>
+            <p><strong>Body:</strong> {body}</p>
+            <h6>Author: {author}</h6>
+
+
+
         </div>
     );
 }
